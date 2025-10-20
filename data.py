a8ecb98d80235fbf435ebbf80dd5c23cefc1b739
alanfortis16 <alancherkasov03@gmail.com>
2025-10-20 14:13:59 -0400

    data.py

--- data.py
+++ data.py
@@ -1,14 +1,18 @@
 from datetime import datetime
+
 def get_days_from_today(date):
+    
     try:
         given_date = datetime.strptime(date, '%Y-%m-%d').date()
         today = datetime.today().date()
         difference = today - given_date
         print("Різниця ", difference.days, "днів")
         return difference.days
+        
     except ValueError:
         print("Помилка: дата має бути у форматі 'РРРР-ММ-ДД'.")
         return None
+
 
 
 get_days_from_today("2021-05-05")
